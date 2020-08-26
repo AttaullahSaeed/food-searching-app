de9034a986fb871fb66d6a402349c8b00d721f7a
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-26 15:19:35 +0500

    Updates

---
+++ precache-manifest.eae7ebb57e4386dd234f5a42b5b61b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ecec2d0adea57bc83fc6807b444e43",
+    "url": "/food-searching-app/index.html"
+  },
+  {
+    "revision": "214cf52e3ff25e03cb33",
+    "url": "/food-searching-app/static/css/main.5f33b05d.chunk.css"
+  },
+  {
+    "revision": "28895e2c6f8e9ac963d2",
+    "url": "/food-searching-app/static/js/2.90436acd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-searching-app/static/js/2.90436acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214cf52e3ff25e03cb33",
+    "url": "/food-searching-app/static/js/main.e8464125.chunk.js"
+  },
+  {
+    "revision": "bc90bf07c8eaea58962c",
+    "url": "/food-searching-app/static/js/runtime-main.285001e7.js"
+  }
+]);
